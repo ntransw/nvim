efd29579e7dfbd9e0ee8b41e4a7e68737312a3be
Nathan Tran <ntranswe@gmail.com>
2024-05-25 02:02:00 -0700

    config stuff

---
+++ init.lua
@@ -0,0 +1,7 @@
+-- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")
+require("lillilac")
+
+local nvim_lsp = require("lspconfig")
+nvim_lsp.gopls.setup({})
+nvim_lsp.rust_analyzer.setup({})
